45b827e32ba1b4f953a09c6060f122de1530efb9
irfanshahh11 <ispc1115@gmail.com>
2021-08-15 18:53:26 +0800

    https://github.com/irfanshahh11/todolist.git

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <Header />
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0.</p>
+  </div>
+  <footer>
+    by @muhammadirfansh | github: irfanshah11
+  </footer>
+</template>
